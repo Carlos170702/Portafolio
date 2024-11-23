3c90636df6f71434b27fd694c5dd413cfda08f4d
Carlos170702 <jbsbsbs97@gmail.com>
2024-11-23 00:04:49 -0600

    se implemento la animacion de FadeIn en las experiencia

--- src/app/about-me/components/CardContextExperience/CardContextExperience.tsx
+++ src/app/about-me/components/CardContextExperience/CardContextExperience.tsx
@@ -1,19 +1,35 @@
+'use client';
+import { SliceIn } from '@/app/components/SliceIn';
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export interface Props {
 	title: string,
 	company: string,
-	description: string
+	description: string,
+	id: number
 }
 
-const CardContextExperience: React.FC<Props> = ({ company, description, title }) => {
+const CardContextExperience: React.FC<Props> = ({ company, description, title, id }) => {
+	const { ref, inView } = useInView({
+		triggerOnce: true,
+	});
+
 	return (
-		<div className="p-6">
-			<h3 className="text-xl font-semibold mb-2">{title}</h3>
-			<h4 className="text-lg font-medium text-muted-foreground mb-2">
-				{company}
-			</h4>
-			<p className="text-muted-foreground">{description}</p>
+		<div ref={ref}>
+			{
+				inView && (
+					<SliceIn direction={id % 2 !== 0 ? 'right' : 'left'}>
+						<div className="p-6">
+							<h3 className="text-xl font-semibold mb-2">{title}</h3>
+							<h4 className="text-lg font-medium text-muted-foreground mb-2">
+								{company}
+							</h4>
+							<p className="text-muted-foreground">{description}</p>
+						</div>
+					</SliceIn>
+				)
+			}
 		</div>
 	);
 };
--- src/app/about-me/components/CardExperience/CardExpirience.tsx
+++ src/app/about-me/components/CardExperience/CardExpirience.tsx
@@ -1,4 +1,3 @@
-import { SliceIn } from '@/app/components/SliceIn';
 import { ExperienceModel } from '../../models';
 import { CardContextExperience } from '../CardContextExperience';
 
@@ -8,25 +7,24 @@ export interface Props {
 
 const CardExperience: React.FC<Props> = ({ experience }) => {
 	return (
-		<SliceIn direction={experience.id % 2 !== 0 ? 'right' : 'left'}>
-			<div className="mb-8 flex flex-col md:flex-row items-center">
-				<div className="flex-1 w-full md:w-1/2 md:pr-8 md:text-right order-2 md:order-1">
-					{experience.id % 2 === 0 && (
-						<CardContextExperience {...experience} />
-					)}
-				</div>
-				<div className="mx-auto md:mx-0 flex items-center justify-center w-12 h-12 rounded-full bg-background border-4 border-border text-primary relative z-10 order-1 md:order-2 bg-white">
-					<span className="text-sm font-semibold">
-						{experience.date.split(" ")[0]}
-					</span>
-				</div>
-				<div className="flex-1 w-full md:w-1/2 md:pl-8 order-2 md:order-3">
-					{experience.id % 2 !== 0 && (
-						<CardContextExperience {...experience} />
-					)}
-				</div>
+
+		<div className="mb-8 flex flex-col md:flex-row items-center">
+			<div className="flex-1 w-full md:w-1/2 md:pr-8 md:text-right order-2 md:order-1">
+				{experience.id % 2 === 0 && (
+					<CardContextExperience {...experience} />
+				)}
 			</div>
-		</SliceIn>
+			<div className="mx-auto md:mx-0 flex items-center justify-center w-12 h-12 rounded-full bg-background border-4 border-border text-primary relative z-10 order-1 md:order-2 bg-white">
+				<span className="text-sm font-semibold">
+					{experience.date.split(" ")[0]}
+				</span>
+			</div>
+			<div className="flex-1 w-full md:w-1/2 md:pl-8 order-2 md:order-3">
+				{experience.id % 2 !== 0 && (
+					<CardContextExperience {...experience} />
+				)}
+			</div>
+		</div>
 	);
 };
 
